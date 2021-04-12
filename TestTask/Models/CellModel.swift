89dcb206665fec3e9d9bdfb44b915e990fa0476e
Stanislav Teslenko <stanislav.teslenko@gmail.com>
2021-04-12 23:11:29 +0300

    initial commit

---
+++ TestTask/Models/CellModel.swift
@@ -0,0 +1,20 @@
+//
+//  CellModel.swift
+//  TestTask
+//
+//  Created by Stanislav Teslenko on 12.04.2021.
+//
+
+import UIKit
+
+struct CellModel {
+    
+    let source: String?
+    let author: String?
+    let title: String?
+    let description: String?
+    let image: UIImage?
+    let url: String?
+
+}
+
